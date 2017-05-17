a2586d86cacbf5f4c6b2dbf7dac411da98a4e468
iggym <iggy.mw@gmail.com>
2017-05-17 00:40:31 -0500

    Initial Commit

---
+++ dum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dum
+//
+//  Created by Iggy on 5/17/17.
+//  Copyright © 2017 iggym. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
