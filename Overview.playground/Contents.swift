3e30aa52f61a17a626c60aeaabd66303eacbd6af
iggym <iggy.mw@gmail.com>
2017-05-18 00:11:05 -0500

    overview and notes

--- Overview.playground/Contents.swift
+++ Overview.playground/Contents.swift
@@ -1,5 +1,21 @@
 //: Playground - noun: a place where people can play
-
+/*:
+ Alamofire is an HTTP networking library written in Swift
+ 
+ Features:
+ Chainable Request / Response Methods
+ URL / JSON / plist Parameter Encoding
+ Upload File / Data / Stream / MultipartFormData
+ Download File using Request or Resume Data
+ Authentication with URLCredential
+ HTTP Response Validation
+ Upload and Download Progress Closures with Progress
+ cURL Command Output
+ Dynamically Adapt and Retry Requests
+ TLS Certificate and Public Key Pinning
+ Network Reachability
+ Comprehensive Unit and Integration Test Coverage
+ */
 import UIKit
 import Alamofire
 import PlaygroundSupport
