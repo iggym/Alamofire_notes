24923b00dac9dd6b5d683e169dc92cadf2f28c74
iggym <iggy.mw@gmail.com>
2017-05-19 02:58:24 -0500

    //GET Request With URL-Encoded Parameters

--- Parameter Encoding.playground/Contents.swift
+++ Parameter Encoding.playground/Contents.swift
@@ -9,3 +9,13 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 
 var str = "Hello, playground"
+//GET Request With URL-Encoded Parameters
+
+let parameters: Parameters = ["foo": "bar"]
+
+// All three of these calls are equivalent
+Alamofire.request("https://httpbin.org/get", parameters: parameters) // encoding defaults to `URLEncoding.default`
+Alamofire.request("https://httpbin.org/get", parameters: parameters, encoding: URLEncoding.default)
+Alamofire.request("https://httpbin.org/get", parameters: parameters, encoding: URLEncoding(destination: .methodDependent))
+
+// https://httpbin.org/get?foo=bar
