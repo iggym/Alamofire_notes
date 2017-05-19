dc82143544023bfa2f36c0be43e14204016b9f81
iggym <iggy.mw@gmail.com>
2017-05-19 03:01:41 -0500

    parameters POST Request with JSON-Encoded Parameters

--- Parameter Encoding.playground/Pages/Overview.xcplaygroundpage/Contents.swift
+++ Parameter Encoding.playground/Pages/Overview.xcplaygroundpage/Contents.swift
@@ -38,3 +38,23 @@ Alamofire.request("https://httpbin.org/post", method: .post, parameters: paramet
 Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters, encoding: URLEncoding.httpBody)
 
 // HTTP body: foo=bar&baz[]=a&baz[]=1&qux[x]=1&qux[y]=2&qux[z]=3
+
+
+JSON Encoding
+
+The JSONEncoding type creates a JSON representation of the parameters object, which is set as the HTTP body of the request. The Content-Type HTTP header field of an encoded request is set to application/json.
+
+POST Request with JSON-Encoded Parameters
+
+let parameters: Parameters = [
+    "foo": [1,2,3],
+    "bar": [
+        "baz": "qux"
+    ]
+]
+
+// Both calls are equivalent
+Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters, encoding: JSONEncoding.default)
+Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters, encoding: JSONEncoding(options: []))
+
+// HTTP body: {"foo": [1, 2, 3], "bar": {"baz": "qux"}}
