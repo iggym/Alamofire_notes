7e4ec425c7810ee2d6864f9bd40169842076f4d1
iggym <iggy.mw@gmail.com>
2017-05-19 02:45:04 -0500

    more imports

--- Response Caching.playground/Contents.swift
+++ Response Caching.playground/Contents.swift
@@ -1,5 +1,11 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Alamofire
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+
 
 var str = "Hello, playground"
