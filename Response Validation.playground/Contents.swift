1b238c3fba4acd29c754a5600808507df8ff9f73
iggym <iggy.mw@gmail.com>
2017-05-19 02:54:20 -0500

    Manual Validation

--- Response Validation.playground/Contents.swift
+++ Response Validation.playground/Contents.swift
@@ -9,3 +9,16 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 
 var str = "Hello, playground"
+//Manual Validation
+
+Alamofire.request("https://httpbin.org/get")
+    .validate(statusCode: 200..<300)
+    .validate(contentType: ["application/json"])
+    .responseData { response in
+        switch response.result {
+        case .success:
+            print("Validation Successful")
+        case .failure(let error):
+            print(error)
+        }
+}
