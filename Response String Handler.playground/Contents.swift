6a6ef6e0d3ed99722dcf95db8efc4d08cdaaf1ef
iggym <iggy.mw@gmail.com>
2017-05-19 02:40:05 -0500

    Response String Handler

--- Response String Handler.playground/Contents.swift
+++ Response String Handler.playground/Contents.swift
@@ -7,3 +7,9 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 var str = "Hello, playground"
+
+
+Alamofire.request("https://httpbin.org/get").responseString { response in
+    print("Success: \(response.result.isSuccess)")
+    print("Response String: \(response.result.value)")
+}
