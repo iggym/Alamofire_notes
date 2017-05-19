f3a3cc14ff0ce9aeaaf34cf17eba7c281c796059
iggym <iggy.mw@gmail.com>
2017-05-19 02:42:47 -0500

    responseJSON handler uses the responseJSONSerializer

--- Response JSON Handler.playground/Contents.swift
+++ Response JSON Handler.playground/Contents.swift
@@ -5,5 +5,15 @@ import Alamofire
 import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
+/*:
+ The responseJSON handler uses the responseJSONSerializer to convert the Data returned by the server into an Any type using the specified JSONSerialization.ReadingOptions. If no errors occur and the server data is successfully serialized into a JSON object, the response Result will be a .success and the value will be of type Any.
+ */
 
-var str = "Hello, playground"
+
+Alamofire.request("https://httpbin.org/get").responseJSON { response in
+    debugPrint(response)
+    
+    if let json = response.result.value {
+        print("JSON: \(json)")
+    }
+}
