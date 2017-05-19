4c393cc8911dcd0067287b13d8df9d432c5d04f9
iggym <iggy.mw@gmail.com>
2017-05-19 02:57:25 -0500

    HTTP Methods

--- HTTP Methods.playground/Contents.swift
+++ HTTP Methods.playground/Contents.swift
@@ -9,3 +9,9 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 
 var str = "Hello, playground"
+
+Alamofire.request("https://httpbin.org/get") // method defaults to `.get`
+
+Alamofire.request("https://httpbin.org/post", method: .post)
+Alamofire.request("https://httpbin.org/put", method: .put)
+Alamofire.request("https://httpbin.org/delete", method: .delete)
