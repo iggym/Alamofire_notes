a2d6d2bf733ac4abef814c0bbb3ca5cb25db2c7d
iggym <iggy.mw@gmail.com>
2017-05-19 02:56:27 -0500

    Response Handler Queue

--- Response Handler Queue.playground/Contents.swift
+++ Response Handler Queue.playground/Contents.swift
@@ -9,3 +9,15 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 
 var str = "Hello, playground"
+/*:
+ Response Handler Queue
+ 
+ Response handlers by default are executed on the main dispatch queue. However, a custom dispatch queue can be provided instead.
+ */
+
+
+let utilityQueue = DispatchQueue.global(qos: .utility)
+
+Alamofire.request("https://httpbin.org/get").responseJSON(queue: utilityQueue) { response in
+print("Executing response handler on utility queue")
+}
