d60c1b9fe114eae6727f1b480cb4aadfc9c11f4b
iggym <iggy.mw@gmail.com>
2017-05-19 02:41:37 -0500

    Response handlers can even be chained:

--- Chained Response Handlers.playground/Contents.swift
+++ Chained Response Handlers.playground/Contents.swift
@@ -1,5 +1,21 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Alamofire
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
 
 var str = "Hello, playground"
+//Chained Response Handlers
+
+//Response handlers can even be chained:
+
+Alamofire.request("https://httpbin.org/get")
+.responseString { response in
+    print("Response String: \(response.result.value)")
+    }
+    .responseJSON { response in
+        print("Response JSON: \(response.result.value)")
+}
