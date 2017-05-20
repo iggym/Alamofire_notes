76e045ddd7206254ab1be0311c99854616ad0b07
iggym <iggy.mw@gmail.com>
2017-05-20 18:58:45 -0500

    Downloading Data to a File

--- Parameter Encoding.playground/Pages/Custom Encoding.xcplaygroundpage/Contents.swift
+++ Parameter Encoding.playground/Pages/Custom Encoding.xcplaygroundpage/Contents.swift
@@ -8,56 +8,26 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 
 
-var str = "Hello, playground"
-//GET Request With URL-Encoded Parameters
+struct JSONStringArrayEncoding: ParameterEncoding {
+    private let array: [String]
+    
+    init(array: [String]) {
+        self.array = array
+    }
+    
+    func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
+        var urlRequest = try urlRequest.asURLRequest()
+        
+        let data = try JSONSerialization.data(withJSONObject: array, options: [])
+        
+        if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
+        
+        urlRequest.httpBody = data
+        
+        return urlRequest
+    }
+}
 
-let parametersGet: Parameters = ["foo": "bar"]
-
-// All three of these calls are equivalent
-Alamofire.request("https://httpbin.org/get", parameters: parametersGet) // encoding defaults to `URLEncoding.default`
-Alamofire.request("https://httpbin.org/get", parameters: parametersGet, encoding: URLEncoding.default)
-Alamofire.request("https://httpbin.org/get", parameters: parametersGet, encoding: URLEncoding(destination: .methodDependent))
-
-// https://httpbin.org/get?foo=bar
-
-//POST Request With URL-Encoded Parameters
-
-let parameters: Parameters = [
-    "foo": "bar",
-    "baz": ["a", 1],
-    "qux": [
-        "x": 1,
-        "y": 2,
-        "z": 3
-    ]
-]
-
-// All three of these calls are equivalent
-Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters)
-Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters, encoding: URLEncoding.default)
-Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters, encoding: URLEncoding.httpBody)
-
-// HTTP body: foo=bar&baz[]=a&baz[]=1&qux[x]=1&qux[y]=2&qux[z]=3
-
-/*:
- JSON Encoding
- 
- The JSONEncoding type creates a JSON representation of the parameters object, which is set as the HTTP body of the request. The Content-Type HTTP header field of an encoded request is set to application/json.
- 
- POST Request with JSON-Encoded Parameters
- 
- */
-
-let parameters: Parameters = [
-    "foo": [1,2,3],
-    "bar": [
-        "baz": "qux"
-    ]
-]
-
-// Both calls are equivalent
-Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters, encoding: JSONEncoding.default)
-Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters, encoding: JSONEncoding(options: []))
-
-// HTTP body: {"foo": [1, 2, 3], "bar": {"baz": "qux"}}
 //: [Next](@next)
