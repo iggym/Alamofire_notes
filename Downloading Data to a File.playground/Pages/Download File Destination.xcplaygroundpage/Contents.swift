e3377bf710341600eba7e6c8b6e436ddd3540343
iggym <iggy.mw@gmail.com>
2017-05-20 19:19:09 -0500

    downloading files

--- Downloading Data to a File.playground/Pages/Download File Destination.xcplaygroundpage/Contents.swift
+++ Downloading Data to a File.playground/Pages/Download File Destination.xcplaygroundpage/Contents.swift
@@ -7,3 +7,19 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 var str = "Hello, playground"
+var imageTmp: UIImage?
+var imageView: UIImageView?
+
+Alamofire.download("https://httpbin.org/image/png").responseData { response in
+    if let data = response.result.value {
+        let image = UIImage(data: data)
+        imageTmp = image
+        
+        print(data)
+        var imageView: UIImageView = UIImageView(image: imageTmp)
+    }
+}
+
+
+
+
