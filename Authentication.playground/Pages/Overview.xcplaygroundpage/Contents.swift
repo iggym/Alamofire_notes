5aaece8bf9ff1e81eda4a1388d40c022def6847c
iggym <iggy.mw@gmail.com>
2017-05-20 19:04:45 -0500

    * Authentication notes

---
+++ Authentication.playground/Pages/Overview.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+
+/*:
+ Authentication is handled on the system framework level by URLCredential and URLAuthenticationChallenge.
+ 
+ Supported Authentication Schemes
+ 
+ * HTTP Basic
+ * HTTP Digest
+ * Kerberos
+ * NTLM
+
+ */
+import UIKit
+import Alamofire
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+
+
+var str = "Hello, playground"
