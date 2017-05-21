a50dcbce53fd7fccc88b67edae5af5033a68bb53
iggym <iggy.mw@gmail.com>
2017-05-20 19:30:14 -0500

    session manager

--- Session Manager.playground/Contents.swift
+++ Session Manager.playground/Contents.swift
@@ -7,3 +7,8 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 var str = "Hello, playground"
+
+Alamofire.request("https://httpbin.org/get")
+
+let sessionManager = Alamofire.SessionManager.default
+sessionManager.request("https://httpbin.org/get")
