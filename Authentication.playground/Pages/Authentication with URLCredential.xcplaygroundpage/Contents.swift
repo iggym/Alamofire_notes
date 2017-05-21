05196d753b194ff2980e287f9f04ec5ede46165a
iggym <iggy.mw@gmail.com>
2017-05-20 19:12:12 -0500

    Authentication with URLCredential

--- Authentication.playground/Pages/Authentication with URLCredential.xcplaygroundpage/Contents.swift
+++ Authentication.playground/Pages/Authentication with URLCredential.xcplaygroundpage/Contents.swift
@@ -7,6 +7,17 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 
-var str = "Hello, playground"
+//Authentication with URLCredential
+
+let user = "user"
+let password = "password"
+
+let credential = URLCredential(user: user, password: password, persistence: .forSession)
+
+Alamofire.request("https://httpbin.org/basic-auth/\(user)/\(password)")
+    .authenticate(usingCredential: credential)
+    .responseJSON { response in
+        debugPrint(response)
+}
 
 //: [Next](@next)
